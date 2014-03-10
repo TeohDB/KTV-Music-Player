9c7a2a583033a5374cce322827a81f5de8f5095e
Narha <vsigbr12@student.aau.dk>
2014-03-10 05:44:27 +0100

    Basic Music player
    button 1 = select file, button 2 = play, button 3 = stop

---
+++ KTVPlayer/KTVPlayer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        MPlayer player = new MPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.play();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            label1.Text = openFileDialog1.FileName;
+            player.open(openFileDialog1.FileName);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.stop();
+        }
+    }
+}
